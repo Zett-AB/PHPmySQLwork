54d25bb5dafb767653e298629b6b22bbd2ebee5d
Alexander <formysite.ru@yandex.ru>
2022-01-20 11:50:07 +0300

    first_step_lesson

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    $nickname=" Александр!";
+    $hello="Привет, ";
+    $hellow="Продолжаем изучение PHP, здесь PHP и MySQL.";
+    echo $hello.$nickname."<br>".$hellow;
+
+    $mysqli= new mysqli('localhost', 'root', '', 'kinomonster');
+
+?>
